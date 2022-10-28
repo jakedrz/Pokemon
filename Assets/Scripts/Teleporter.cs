569b2425f62f234c281d32044220612f6578cfd9
Jake Drzewiecki <jake.drzewiecki@gmail.com>
2022-10-28 12:02:44 -0500

    wip screen fader

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Teleporter : MonoBehaviour
 {
     public GameObject destination;
+    public Image fader;
+    private Image imageFader;
     void Start()
     {
-        
+        Color fadeColor = Color.clear;
+        imageFader = fader.GetComponent<Image>();
+        imageFader.color = fadeColor;
+        Debug.Log(imageFader.color);
     }
 
     // Update is called once per frame
